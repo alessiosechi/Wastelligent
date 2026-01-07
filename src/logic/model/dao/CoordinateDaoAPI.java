9ded173f45ddf22460a4c1c680d825f73238e2c1
Alessio <alessio.sechi@students.uniroma2.eu>
2026-01-07 17:07:08 +0100

    Ultima modifica

--- src/logic/model/dao/CoordinateDaoAPI.java
+++ src/logic/model/dao/CoordinateDaoAPI.java
@@ -6,27 +6,46 @@ import com.google.gson.JsonObject;
 import logic.model.domain.Coordinate;
 
 import com.google.gson.JsonArray;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Properties;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class CoordinateDaoAPI implements CoordinateDao {
-	private static final String CHIAVE_API_GEOCODING = "c1e13be9a63f4c13b120089500a57f1b"; // chiave API
 	private static final Logger logger = Logger.getLogger(CoordinateDaoAPI.class.getName());
+	private static final String PROPERTIES_FILE = "resources/config-opencage.properties";
+
+	private String caricaChiave() {
+		try (InputStream input = new FileInputStream(PROPERTIES_FILE)) {
+			Properties properties = new Properties();
+			properties.load(input);
+			return properties.getProperty("OPENCAGE_API_KEY");
+		} catch (IOException e) {
+			logger.severe("Errore caricamento chiave OpenCage: " + e.getMessage());
+			return null;
+		}
+	}
 
 	@Override
 	public Coordinate ottieniCoordinate(String posizioneTesto) {
+		String key = caricaChiave();
+		if (key == null) return null;
+
 		try {
 			// codifico in un formato URL-safe
 			String posizioneCodificata = URLEncoder.encode(posizioneTesto, "UTF-8");
 
 			// creo la stringa da usare per la richiesta HTTP
 			String urlStringa = String.format("https://api.opencagedata.com/geocode/v1/json?q=%s&key=%s",
-					posizioneCodificata, CHIAVE_API_GEOCODING);
+					posizioneCodificata, key);
 
 			URI uri = new URI(urlStringa);
 			URL url = uri.toURL();
@@ -66,9 +85,12 @@ public class CoordinateDaoAPI implements CoordinateDao {
 
 	@Override
 	public String ottieniPosizione(Coordinate coordinate) {
+		String key = caricaChiave();
+		if (key == null) return null;
+
 		try {
 			String urlStringa = String.format("https://api.opencagedata.com/geocode/v1/json?q=%f,+%f&key=%s",
-					coordinate.getLatitudine(), coordinate.getLongitudine(), CHIAVE_API_GEOCODING);
+					coordinate.getLatitudine(), coordinate.getLongitudine(), key);
 
 			URI uri = new URI(urlStringa);
 			URL url = uri.toURL();
