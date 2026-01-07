4b19a051b969b3c81dc03617951ce13624d8d63b
Alessio <alessio.sechi@students.uniroma2.eu>
2026-01-07 16:39:25 +0100

    Ultima modifica

--- src/logic/model/dao/RiscattoDaoDatabase.java
+++ src/logic/model/dao/RiscattoDaoDatabase.java
@@ -3,7 +3,6 @@ package logic.model.dao;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
